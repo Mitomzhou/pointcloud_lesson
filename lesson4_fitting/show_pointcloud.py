7078847beb35556cfb4287896653290d5496b3be
mitom <zhouyong_monky@163.com>
2021-10-16 18:04:48 +0800

    点云可视化

--- lesson4_fitting/show_pointcloud.py
+++ lesson4_fitting/show_pointcloud.py
@@ -1,33 +1,24 @@
+import os
 import numpy as np
-import mayavi.mlab
+import struct
+import open3d
 
-# 激光点云可视化：https://blog.csdn.net/r1141207831/article/details/105381707
+def read_bin_velodyne(path):
+    pc_list=[]
+    with open(path,'rb') as f:
+        content=f.read()
+        pc_iter=struct.iter_unpack('ffff',content)
+        for idx,point in enumerate(pc_iter):
+            pc_list.append([point[0],point[1],point[2]])
+    return np.asarray(pc_list,dtype=np.float32)
 
-# lidar_path换成自己的.bin文件路径
-pointcloud = np.fromfile(str("../data/000004.bin"), dtype=np.float32, count=-1).reshape([-1, 4])
+def main():
+    bin_path='../data/000004.bin'
+    pcd=open3d.open3d.geometry.PointCloud()
+    example=read_bin_velodyne(bin_path)
+    # From numpy to Open3D
+    pcd.points= open3d.open3d.utility.Vector3dVector(example)
+    open3d.open3d.visualization.draw_geometries([pcd])
 
-x = pointcloud[:, 0]  # x position of point
-y = pointcloud[:, 1]  # y position of point
-z = pointcloud[:, 2]  # z position of point
-
-r = pointcloud[:, 3]  # reflectance value of point
-d = np.sqrt(x ** 2 + y ** 2)  # Map Distance from sensor
-
-degr = np.degrees(np.arctan(z / d))
-
-vals = 'height'
-if vals == "height":
-    col = z
-else:
-    col = d
-
-fig = mayavi.mlab.figure(bgcolor=(0, 0, 0), size=(640, 500))
-mayavi.mlab.points3d(x, y, z,
-                     col,  # Values used for Color
-                     mode="point",
-                     colormap='spectral',  # 'bone', 'copper', 'gnuplot'
-                     # color=(0, 1, 0),   # Used a fixed (r,g,b) instead
-                     figure=fig,
-                     )
-
-mayavi.mlab.show()
+if __name__=="__main__":
+    main()
