c218e9e9cfee8b6a872f02829ab231dd25bef772
Mitomzhou <zhouyong_monky@163.com>
2021-10-26 22:40:19 +0800

    添加注释

--- lesson5_pointnet/pointnet1_cls_train.py
+++ lesson5_pointnet/pointnet1_cls_train.py
@@ -10,7 +10,7 @@ from pointnet1_cls import PointNetCls, PointNetLoss
 
 
 def main():
-    # 超参数设置
+    """ 超参数设置 """
     USE_CUDA = True
     NUM_CLASS = 40
     BATCH_SIZE = 32
@@ -21,7 +21,7 @@ def main():
     # 数据加载
     data_path = "/home/mitom/3DPointCloud/data/modelnet40_normal_resampled"
     train_dataset = ModelNetDataSet(root=data_path, split='train')
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8, drop_last=True)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8, drop_last=True) # drop_last=True:最后一个batch数据不完整就删除
 
     classifier = PointNetCls(k=NUM_CLASS)
     critertion = PointNetLoss()
@@ -35,6 +35,7 @@ def main():
     # SGD
     # optimizer = torch.optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
 
+    # 调整学习率
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.7)
 
     # 训练
@@ -42,17 +43,20 @@ def main():
         mean_correct = []
         classifier = classifier.train()
 
-        scheduler.step()
-        for batch_id, (points, target) in tqdm(enumerate(train_dataloader, 0), total=len(train_dataloader), smoothing=0.9):
+        for batch_id, (points, target) in tqdm(enumerate(train_dataloader, 0), total=len(train_dataloader), smoothing=0.9): # tqdm进度条
             optimizer.zero_grad()
 
-            points = points[:,:,0:3]
-            points = points.transpose(2,1)
+            points = points[:,:,0:3] # 取 x y z
+            points = points.transpose(2,1) # (B,N,D) -> (B,D,N)
             if USE_CUDA:
                 points, target = points.cuda(), target.cuda()
 
             pred, trans_feat = classifier(points)
             loss = critertion(pred, target.long(), trans_feat)
+
+            # pred.shape: (B, 40)
+            print(pred.shape)
+            # pred_choice: 返回分数最大的index  (B,)
             pred_choice = pred.data.max(1)[1]
 
             correct = pred_choice.eq(target.long().data).cpu().sum()
@@ -60,6 +64,7 @@ def main():
             loss.backward()
             optimizer.step()
 
+        scheduler.step()
         train_instance_acc = np.mean(mean_correct)
         print("Train Instance Accuracy: %f" % train_instance_acc)
 
