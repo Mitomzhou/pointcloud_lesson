17a33e0ce8069eb230779ca7b1ccca5d3b62de3c
Mitomzhou <zhouyong_monky@163.com>
2021-11-03 17:39:28 +0800

    pointnet++ MSG

--- lesson5_pointnet/pointnet2_cls_msg.py
+++ lesson5_pointnet/pointnet2_cls_msg.py
@@ -3,11 +3,86 @@
 # File  : pointnet2_cls_msg
 # Description:  MSG(Multi-scale Grouping) PointNet++分类
 
+import datetime
+import os.path
+import logging
+from pathlib import Path
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# TODO
+from pointnet2_utils import SetAbstraction, SetAbstractionMsg
+
+
+class PointNet2ClsMsg(nn.Module):
+    """
+    MSG(Multi-scale Grouping) PointNet++分类
+    """
+    def __init__(self, num_class, normal_channel=True):
+        super(PointNet2ClsMsg, self).__init__()
+        in_channel = 3 if normal_channel else 0
+        self.normal_channel = normal_channel
+        self.sa1 = SetAbstractionMsg(npoint=512, radius_list=[0.1, 0.2, 0.4], nsample_list=[16, 32, 128], in_channel=in_channel, mlp_list=[[32,32,64],[64,64,128],[64,96,128]])
+        self.sa2 = SetAbstractionMsg(npoint=128, radius_list=[0.2, 0.4, 0.8], nsample_list=[32, 64, 128], in_channel=320, mlp_list=[[64,64,128],[128,128,256],[128,128,256]])
+        # group_all=True 所有点全局特征，不需要npoint, radius, nsample参数
+        self.sa3 = SetAbstraction(npoint=None, radius=None, nsample=None, in_channel=128+256+256 + 3, mlp=[256, 512, 1024], group_all=True)
+        self.fc1 = nn.Linear(1024, 512)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.drop1 = nn.Dropout(0.4)
+        self.fc2 = nn.Linear(512, 256)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.drop2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(256, num_class)
+
+
+    def forward(self, xyz):
+        """
+        :param xyz: (B, C, N) C=3
+        :return:
+            x: (B, num_class)
+            sal3_new_points: (B, last_mlp[-1]=1024, 1)
+        """
+        B, _, _ = xyz.shape
+        if self.normal_channel:
+            norm = xyz[:, 3:, :]
+            xyz = xyz[:, :3, :]
+        else:
+            norm = None
+        sal1_new_xyz, sal1_new_points = self.sa1(xyz, norm)
+        print("sal1_new_xyz", sal1_new_xyz.shape, "sal1_new_points", sal1_new_points.shape)
+
+        sal2_new_xyz, sal2_new_points = self.sa2(sal1_new_xyz, sal1_new_points)
+        print("sal2_new_xyz", sal2_new_points.shape, "sal2_new_points", sal2_new_points.shape)
+
+        sal3_new_xyz, sal3_new_points = self.sa3(sal2_new_xyz, sal2_new_points)
+        print("sal3_new_xyz", sal3_new_xyz.shape, "sal3_new_points", sal3_new_points.shape)
+
+        x = sal3_new_points.view(B, 1024)
+        x = self.drop1(F.relu(self.bn1(self.fc1(x))))
+        x = self.drop2(F.relu(self.bn2(self.fc2(x))))
+        x = self.fc3(x)
+        x = F.log_softmax(x, -1)
+
+        return x, sal3_new_points
+
+
+class PointNet2ClsLoss(nn.Module):
+    def __init__(self):
+        super(PointNet2ClsLoss, self).__init__()
+
+    def forward(self, pred, target, trans_feat):
+        total_loss = F.nll_loss(pred, target)
+        return total_loss
+
+
+if __name__ == "__main__":
+    input = torch.rand(4, 6, 1000)
+    classifier = PointNet2ClsMsg(10)
+    x, y = classifier(input)
+    print(x.shape)
+    print(y.shape)
+
+
 
 
--- lesson5_pointnet/pointnet2_utils.py
+++ lesson5_pointnet/pointnet2_utils.py
@@ -8,10 +8,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
 class SetAbstraction(nn.Module):
     """
-    set abstraction
+    set abstraction (for Single-scale Grouping)
     """
     def __init__(self, npoint, radius, nsample, in_channel, mlp, group_all):
         """
@@ -61,6 +60,77 @@ class SetAbstraction(nn.Module):
         new_points = torch.max(new_points, 2)[0]
         new_xyz = new_xyz.permute(0, 2, 1)
         return new_xyz, new_points
+
+
+class SetAbstractionMsg(nn.Module):
+    """
+    set abstraction (for Multi-scale Grouping)
+    """
+    def __init__(self, npoint, radius_list, nsample_list, in_channel, mlp_list):
+        """
+        :param npoint: FPS采样点
+        :param radius_list: eg. [0.1, 0.2, 0.4]
+        :param nsample_list: eg. [16, 32, 128]
+        :param in_channel:
+        :param mlp_list: [[32,32,64], [64,64,128],[64,96,128]]
+        """
+        super(SetAbstractionMsg, self).__init__()
+        self.npoint = npoint
+        self.radius_list = radius_list
+        self.nsample_list = nsample_list
+        self.conv_blocks = nn.ModuleList()
+        self.bn_blocks = nn.ModuleList()
+
+        # 初始化 conv+bn blocks
+        for i in range(len(mlp_list)):
+            convs = nn.ModuleList()
+            bns = nn.ModuleList()
+            last_channel = in_channel + 3
+            for out_channel in mlp_list[i]:
+                convs.append(nn.Conv2d(last_channel, out_channel, 1))
+                bns.append(nn.BatchNorm2d(out_channel))
+                last_channel = out_channel
+            self.conv_blocks.append(convs)
+            self.bn_blocks.append(bns)
+
+    def forward(self, xyz, points):
+        """
+        :param xyz: 点坐标信息 (B,3,N)
+        :param points: 点其他信息 (B,D,N)
+        :return:
+            new_xyz: 采样点位置信息 (B,3,S)
+            new_points_concat: 采样点feature (B,D',S)
+        """
+        xyz = xyz.permute(0, 2, 1)
+        if points is not None:
+            points = points.permute(0, 2, 1)
+
+        B, N, C = xyz.shape
+        S = self.npoint
+        new_xyz = index_points(xyz, farthest_point_sample(xyz, S))
+        new_points_list = []
+        for i, radius in enumerate(self.radius_list):
+            K = self.nsample_list[i]
+            group_idx = query_ball_point(radius, K, xyz, new_xyz)
+            grouped_xyz = index_points(xyz, group_idx)
+            grouped_xyz -= new_xyz.view(B, S, 1, C)
+            if points is not None:
+                grouped_points = index_points(points, group_idx)
+                grouped_points = torch.cat([grouped_points, grouped_xyz], dim=-1)
+            else:
+                grouped_points = grouped_xyz
+
+            grouped_points = grouped_points.permute(0, 3, 2, 1)  # [B, D, K, S]
+            for j in range(len(self.conv_blocks[i])):
+                conv = self.conv_blocks[i][j]
+                bn = self.bn_blocks[i][j]
+                grouped_points = F.relu(bn(conv(grouped_points)))
+            new_points = torch.max(grouped_points, 2)[0]  # [B, D', S]
+            new_points_list.append(new_points)
+
+        new_xyz = new_xyz.permute(0, 2, 1)
+        new_points_concat = torch.cat(new_points_list, dim=1)
+        return new_xyz, new_points_concat
 
 
 def farthest_point_sample(xyz, npoint):
